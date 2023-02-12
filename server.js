8e21ee1664927c6df4fbeda904b4ec5fd03516d3
xSaeedii <smk1995m@gmai.com>
2023-02-12 12:35:14 +0300

    install and implement cors middleware

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200; 
+const cors = require('cors');
+const path = require('path');
 
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 8000; 
+
+server.use(cors);
+server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on http://localhost:${PORT}`)
+})
